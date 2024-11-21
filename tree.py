07410122fc6282d38961cb28484230c64655a09c
KevinGnanaraj <kpg21@ic.ac.uk>
2024-11-21 17:51:26 +0000

    Random Forest Final
    fixed random seed ready for conversion to ipynb

--- tree.py
+++ tree.py
@@ -33,20 +33,20 @@ def load_data(n, val=False):
     
 
 
-def train_model():
+def train_eval_model():
 
     # Load Data & Split the dataset into training and testing sets
-    n=15
+    n=12
     X_train, y_train, X_test, y_test = load_data(n, val=False)
 
     # Feature Selection (Keep 100 best features based on statistical tests)
 
-    i=11
+    i=8
     pca = PCA(n_components = i)
     X_train_pca = pca.fit_transform(X_train)
     X_test_pca = pca.transform(X_test) 
 
-    rf_tree = RandomForestClassifier(n_estimators=2000, random_state=50, n_jobs=-1)
+    rf_tree = RandomForestClassifier(n_estimators=2000, random_state = 50, n_jobs=-1)
 
 
     rf_tree.fit(X_train_pca, y_train)       ## Make predictions on the test set
@@ -56,16 +56,34 @@ def train_model():
     print(f"Validation accuracy: {accuracy_score(y_test, y_pred)} for n={n} and n_components = {i}")
 
 
-    # Accuracy for different levels of dimensionality
 
-    # n_values = [9, 12, 15, 18]
-    # accuracy = [0.959, 0.945, 0.806, 0.5894]
+train_eval_model()
 
-
-
-
+# n_values = [9, 12, 15]
+# accuracy = [0.959, 0.945, 0.806]
+# threshold = [0.95, 0.925, 0.9]
 
 
-train_model()
+# variance_list_15 = [0.80625, 0.80633, 0.80066, 0.8035, 0.8025]
+# variance_list_9 = [0.9589, 0.95944, 0.95916, 0.95944, 0.95916]
+# variance_list_12 = [0.9306, 0.9308, 0.931575, 0.92979, 0.92995]
+
+# var = [np.ptp(variance_list_9), np.ptp(variance_list_12), np.ptp(variance_list_15)]
+
+
+# plt.errorbar(n_values, accuracy, marker = 'o', label = "Model Accuracy", yerr = var, ecolor="grey", capsize=3)
+# plt.plot(n_values, threshold, marker = 'x', linestyle = "--", label = "Threshold Accuracy", color = 'red')
+# plt.xlabel("Dimensionality of Feature Space")
+# plt.ylabel("Test Accuracy")
+# plt.legend(loc="best")
+# plt.show()
+
+
+
+
+
+
+
+
 
     
